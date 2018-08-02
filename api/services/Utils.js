183108d93747b3232df07714ca0373fddc974c1b
Alexander Wilson <acwilson96@gmail.com>
2018-08-02 22:36:49 +0100

    Utilities Service

---
+++ api/services/Utils.js
@@ -0,0 +1,30 @@
+/**
+ * Utils
+ *
+ * @description :: Service that exposes utility methods. E.G. Current date in DD/MM/YYYY format
+ */
+
+module.exports = {
+
+  currDate: function () {
+    var now = new Date();
+    var dd = now.getDate();
+    var mm = now.getMonth()+1; //January is 0!
+    var yyyy = now.getFullYear();
+    if(dd<10) {
+      dd = '0'+dd;
+    }
+    if(mm<10) {
+      mm = '0'+ mm;
+    }
+    return (dd + '/' + mm + '/' + yyyy);
+  },
+
+  returnJsonError: function (err) {
+    return {
+      error: true,
+      warning: false,
+      content: err
+    };
+  }
+};
