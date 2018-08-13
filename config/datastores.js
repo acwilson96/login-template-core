2be1183ff316b13285a5b5e8a38b9aee72506502
Alexander Wilson <acwilson96@gmail.com>
2018-08-13 15:12:45 +0100

    Configure Database in environment configs.

--- config/datastores.js
+++ config/datastores.js
@@ -51,9 +51,6 @@ module.exports.datastores = {
     // adapter: 'sails-mysql',
     // url: 'mysql://user:password@host:port/database',
 
-    adapter: 'sails-mongo',
-    url: 'mongodb://root@localhost/login-template'
-
   },
 
 
